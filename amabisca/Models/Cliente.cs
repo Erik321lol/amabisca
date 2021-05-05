f311dcc0572b1e6b7c621fec5029ebd80e16e9ee
lucho96alkatraz <jmarcial96@hotmail.com>
2021-05-05 03:42:22 -0600

    Finalizacion de vista Reportes, y sus pdf´s

--- amabisca/Models/Cliente.cs
+++ amabisca/Models/Cliente.cs
@@ -7,9 +7,22 @@ namespace amabisca.Models
 {
     public partial class Cliente
     {
-        public Cliente()
+
+        public static List<Cliente> clientes = new List<Cliente>();
+
+        public Cliente(object v) { }
+
+        public Cliente(int cod_cliente, String nombre1, String nombre2, String apellido1, String apellido2, String nit, String telefono, String direccion)
         {
             Venta = new HashSet<Ventum>();
+            CodCliente = cod_cliente;
+            Nombre1 = nombre1;
+            Nombre2 = nombre2;
+            Apellido1 = apellido1;
+            Apellido2 = apellido2;
+            Nit = nit;
+            Telefono = telefono;
+            Direccion = direccion;
         }
 
         public int CodCliente { get; set; }
